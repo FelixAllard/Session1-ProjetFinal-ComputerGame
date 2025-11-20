02946abab3c8e9e3f0d850db3b4dbb0047731ab2
Felix Allard <xilef992@gmail.com>
2025-11-20 01:21:24 -0500

    Finaly! Things Work!

--- Assets/Communication/CommunicationManager.cs
+++ Assets/Communication/CommunicationManager.cs
@@ -1,7 +1,17 @@
+using System;
 using UnityEngine;
 
 public class CommunicationManager : MonoBehaviour
 {
+    static CommunicationManager Instance;
+    private void Awake()
+    {
+        if(Instance!=null)
+            Destroy(gameObject);
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,6 +20,49 @@ public class CommunicationManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void ReceivedMessageFromRemote(string msg)
+    {
+        if (msg == "BUTTON_A")
+        {
+            Received_A_ButtonPress();
+        }
+        else if (msg == "BUTTON_B"){
+            Received_B_ButtonPress();
+        }
+        else if (msg == "BUTTON_C")
+        {
+            Received_C_ButtonPress();
+        }
+        else if (msg == "BUTTON_D")
+        {
+            Received_D_ButtonPress();
+        }
+        else
+        {
+            Debug.LogError("Received Unidentified Message: " + msg);
+        }
+    }
+
+    public void Received_A_ButtonPress()
+    {
+        
+    }
+
+    public void Received_B_ButtonPress()
+    {
+        
+    }
+
+    public void Received_C_ButtonPress()
+    {
+        
+    }
+
+    public void Received_D_ButtonPress()
     {
         
     }
---
+++ Assets/Communication/ControllerPc/ScenesRemotes/MainMenutRemoteInputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Communication.ControllerPc.ScenesRemotes
+{
+    public class MainMenutRemoteInputController : MonoBehaviour, IRemoteInputController
+    {
+        public void OnButtonA()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnButtonB()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnButtonC()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnButtonD()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
